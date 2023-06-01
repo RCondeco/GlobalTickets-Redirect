24b5fd3edf487c514b4961f2a877654fa6209bd4
RCondeco <106067665+RCondeco@users.noreply.github.com>
2023-06-01 21:45:13 +0100

    text file added and new account notification

--- index.php
+++ index.php
@@ -71,6 +71,16 @@ include './include/autoloader.php';
 <body>
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <?php 
+                if(isset($_GET['error']) && $_GET['error'] == 'created' ) {
+                    ?>
+            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                role="alert">
+                <span class="font-medium">Success</span> Your account has been created, you can login now.
+            </div>
+            <?php
+                }
+            ?>
             <div
                 class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
